ffc3f767c4968798026fc6c0ece17242bed2179e
= <numenor21@gmail.com>
2018-06-26 20:15:31 -0400

    Validate access policies

--- tests/features/AcceptAnswerTest.php
+++ tests/features/AcceptAnswerTest.php
@@ -26,6 +26,53 @@ class AcceptAnswerTest extends FeatureTestCase
         
         $this->seePageIs($comment->post->url)
             ->seeInElement('.answer', $comment->comment);
+    }
 
+    /** @test */
+    function the_non_post_author_dont_see_the_accept_answer_button()
+    {
+        // Arrange
+        $comment = factory(Comment::class)->create([
+            'comment' => 'Esta será la respuesta del post'
+        ]);
+
+        // Act
+        $this->actingAs($this->defaultUser())
+            ->visit($comment->post->url)
+            ->dontSee('Aceptar respuesta');
+    }
+
+    /** @test */
+    function the_non_post_author_cannot_accept_a_comment_as_the_post_answer()
+    {
+        // Arrange
+        $comment = factory(Comment::class)->create([
+            'comment' => 'Esta será la respuesta del post'
+        ]);
+
+        // Act
+        $this->actingAs($this->defaultUser());
+        $this->post(route('comments.accept', $comment));
+
+        // Assert
+        $this->seeInDatabase('posts', [
+            'id' => $comment->post_id,
+            'pending' => true,
+        ]);
+    }
+
+     /** @test */
+    function the_accept_button_is_hidden_when_the_comment_is_already_the_post_answer()
+    {
+        $comment = factory(Comment::class)->create([
+            'comment' => 'Esta será la respuesta del post'
+        ]);
+
+        $this->actingAs($comment->post->user);
+
+        $comment->markAsAnswer();
+
+        $this->visit($comment->post->url)
+            ->dontSee('Aceptar respuesta');
     }
 }
