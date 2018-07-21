2cbab83dfe1ce5e736e6c25cba85f42b565915e0
= <numenor21@gmail.com>
2018-07-21 18:27:36 -0400

    Improvement some changes in layaout: Top Menu, Aside bars

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,21 +1,36 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>
-        {{ $category->exists ? 'Posts de '.$category->name : 'Posts' }}
-    </h1>
 
-    <ul>
-    @foreach($posts as $post)
-        <li>
-            <a href="{{ $post->url }}">
-                {{ $post->title }}
-            </a>
-        </li>
-    @endforeach
-    </ul>
+    <div class="row">
+        <div class="col-md-12">
+            <h1>
+                {{ $category->exists ? 'Posts de '.$category->name : 'Posts' }}
+            </h1>  
+        </div>
+    </div>
 
+    <div class="row">
+        <div class="col-md-2">
+            <h4>Filtros</h4>
+            {!! Menu::make(trans('menu.filters'), 'nav filters') !!}
+
+            <h4>Categorías</h4>
+            {!! Menu::make($categoryItems, 'nav categories') !!}
+        </div>
+    
+        <div class="col-md-10">
+            <ul>
+            @foreach($posts as $post)
+                <li>
+                    <a href="{{ $post->url }}">
+                        {{ $post->title }}
+                    </a>
+                </li>
+            @endforeach
+            </ul>
+        </div>
+    </div>
+    
     {{ $posts->render() }}
-
-    {!! Menu::make($categoryItems, 'nav categories') !!}
 @endsection
